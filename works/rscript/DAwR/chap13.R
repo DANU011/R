d5fb7fb41a9b493fcbd8a3d3369f1c927216e568
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-09-02 21:47:29 +0900

    Create chap13.R

---
+++ works/rscript/DAwR/chap13.R
@@ -0,0 +1,10 @@
+## 기술통계량: 대푯값, 산포, 선형관계
+
+# 대푯값: 평균, 절사평균, 중위수, 백분위수, 사분위수
+# 산포: 최솟값, 최댓값, 범위, 사분범위, 분산, 표준편차
+# 선형관계: 공분산, 상관계수 
+
+library(tidyverse)
+getwd()
+setwd(dir = 'C:\Users\DA\GitHub\R\works\rscript\DAwR\data')
+list.files()
